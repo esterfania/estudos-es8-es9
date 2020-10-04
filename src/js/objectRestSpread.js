6e31980c0b5d02225e5f52b51bd9a7b30d22f6aa
Ester <esterfania@users.noreply.github.com>
2020-10-04 20:54:23 -0300

    Spread

--- src/js/objectRestSpread.js
+++ src/js/objectRestSpread.js
@@ -2,3 +2,7 @@ const { angular, ...resto } = cursos;
 
 console.log(angular);
 console.log(resto);
+
+//voltando para o mesmo nível
+const novosCursos = { ...resto, angular };
+console.log(novosCursos);
