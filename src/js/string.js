0bfb991f65e7f73730868ebe1e887afa48867b54
Ester <esterfania@users.noreply.github.com>
2020-10-04 20:07:30 -0300

    Inclui padEnd

--- src/js/string.js
+++ src/js/string.js
@@ -3,6 +3,14 @@ const lastName = 'Gama';
 
 const fullName = `${name} ${lastName}`;
 
+//padStart
 const nomeCompletoStringPadding = lastName.padStart(name.length + lastName.length, name);
 
 document.getElementById('stringPad').innerHTML = nomeCompletoStringPadding;
+
+//padEnd
+
+const nomeCompletoStringPaddingEnd = name.padStart(name.length + lastName.length, lastName);
+
+document.getElementById('stringPadEnd').innerHTML = nomeCompletoStringPaddingEnd;
+
